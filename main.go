b40748bfe903f295950fa6b91e3f462b033c54fa
najBaby <naja0122@gmail.com>
2019-11-07 11:07:03 +0000

    stabilisation

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-<<<<<<< HEAD
-
-func main() {
-
-}
-=======
 import (
 	"Editor_NaN/controller"
 	"net/http"
@@ -21,5 +15,3 @@ func main (){
 	http.HandleFunc("/", mc.Index)
 	http.ListenAndServe(":8080", nil)
 }
-
->>>>>>> fdfe077cb069498fefae3f385a9239ea9a7f1358
