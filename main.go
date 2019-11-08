23a2082fe051b3946dfddea40f45fc59f40e6ac0
201RichK <amosamospascal@yahoo.com>
2019-11-08 17:46:34 +0000

    rapplelle main configuration

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 )
 
 func main() {
+
+	//run the Db on sync mode
 	err := orm.RunSyncdb("default", false, true)
 	if err != nil {
 		panic(err)
 	}
+
+	//enable session
 	beego.BConfig.WebConfig.Session.SessionOn = true
+
+	//run the app
 	beego.Run()
 }
