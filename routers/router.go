1f4b90c2fdc9798739cf5ce3f0d82403826511f0
201RichK <amosamospascal@yahoo.com>
2019-11-08 17:22:37 +0000

    router configuration

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,14 @@
 package routers
 
 import (
-	"workspace/Editor_NaN/controllers"
+	"Editor_NaN/code/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{}, "get:Index")
+	beego.Router("/login", &controllers.UserController{}, "get:LoginPage;post:Login")
+
+	beego.Router("/send", &controllers.MainController{}, "options,post:Send")
 }
