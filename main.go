05c517ea80db52d7cea2d1e87d693961ab6223fa
201RichK <amosamospascal@yahoo.com>
2019-11-08 17:17:47 +0000

    configuration main

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	_ "workspace/Editor_NaN/routers"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
 func main() {
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		panic(err)
+	}
+	beego.BConfig.WebConfig.Session.SessionOn = true
 	beego.Run()
 }
-
