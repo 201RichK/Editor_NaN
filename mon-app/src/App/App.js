718663d12030eb1f8bd3b51f5d3b9a432a01ec19
Your Name <you@example.com>
2019-11-12 17:52:28 +0000

    latest commit

--- mon-app/src/App/App.js
+++ mon-app/src/App/App.js
@@ -5,6 +5,7 @@ import Exercice from './Exercice/exercice'
 import Timeline from './Timeline/timeline'
 
 function App() {
+
   return (
     <div className="App flex column one">
       <div className="flex row three">
--- mon-app/src/App/Editor/editor.js
+++ mon-app/src/App/Editor/editor.js
@@ -18,7 +18,7 @@ export default class Editor extends Component {
     render = () => {
         return (
             <div id="Editor" className="flex just three column">
-                <EditorAce mode={this.state.lang} theme={this.state.theme} width="100%" fontSize="16px" defaultValue={this.state.program} height="100%"/>
+                <EditorAce rel="ace" mode={this.state.lang} theme={this.state.theme} width="100%" fontSize="16px" defaultValue={this.state.program} height="100%"/>
                 <Console></Console>
             </div>
         )
