1f221698851d5773fd9ed08c72190593f2a94cb7
Amos Pascal Amos <amosamospascal@yahoo.com>
2019-11-15 16:57:15 +0000

    login code envoyer a l'api Golang

--- mon-app/src/App/login/login.js
+++ mon-app/src/App/login/login.js
@@ -2,15 +2,40 @@ import React, {Component} from 'react'
 import './login.css'
 
 export default class Login extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            username: '',
+            password: '',
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({
+            username: event.target.username,
+            password: event.target.password})
+    }
+
+    handleSubmit(event) {
+        console.log(this.state.value)
+       //event.preventDefault();
+    }
+
     render = () => {
         return (
             <div className="vertical-align">
                 <div className="container">
                     <div className="avatar"></div>
-                    <form method="POST">
-                        <input type="text" placeholder="Username" name="username" />   
-                        <input type="password" placeholder="Password" name="password"/>
+                    <form onSubmit={this.handleSubmit()}>
+
+                        <input type="text" placeholder="Username" name="username" onChange={this.handleChange()}/>   
+                        <input type="password" placeholder="Password" name="password" onChange={this.handleChange()}/>
                         <input type="submit" value="Login" />
+
                     </form>
                 </div>
             </div>
