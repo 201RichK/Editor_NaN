0c0463e9fa4ff070901b7b7e4ea1c79e1e0aec43
Amos Pascal Amos <amosamospascal@yahoo.com>
2019-11-15 16:02:09 +0000

    deplacemen de exercice

---
+++ mon-app/src/App/playground/Exercice/exercice.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import './exercice.css'
+
+export default class Exercice extends Component {
+    
+
+
+
+    render = () => {
+        return (
+            <div id="Exercice" >
+                <ul>
+                    <li>
+                        Exercice 1
+                        <ul>
+
+                        </ul>
+                    </li>
+                    <li>
+
+                    </li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        )
+    }
+}
