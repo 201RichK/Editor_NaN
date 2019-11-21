76188b1d74686ab8aad91c14dc682f8f9f6f279c
Your Name <you@example.com>
2019-11-21 13:49:43 +0000

    revert commit

--- EditorApi/routers/router.go
+++ EditorApi/routers/router.go
@@ -2,13 +2,10 @@ package routers
 
 import (
 	"Editor_NaN/EditorApi/controllers"
-
 	"github.com/astaxie/beego"
 )
 
 func init() {
-
-	beego.Router("/ws", &controllers.WebScoketController{}, "get:Handle")
 	beego.Router("/", &controllers.HomeController{}, "get,post:Home")
 	beego.Router("/play", &controllers.HomeController{}, "get:Play")
 	beego.Router("/run", &controllers.ProgramController{}, "post,options:RunProgram")
